ceca321d35d78c3d64092750a6573b44695c0899
JosueSc11 <josue.solano@cua.uam.mx>
2023-05-07 05:17:10 -0600

    statics

--- SMART_PLANNING/urls.py
+++ SMART_PLANNING/urls.py
@@ -17,6 +17,8 @@ from django.contrib.auth import login
 from django.contrib.auth.views import logout_then_login
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -26,3 +28,5 @@ urlpatterns = [
     path('', include('project_pack.urls')),
     path('', include('Panel_Calculo.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
